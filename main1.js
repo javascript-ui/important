3c5bf0a307134b6e5f8e9f2d6806f06682b92af2
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-10-12 05:04:49 +0800

    update

--- main1.js
+++ main1.js
@@ -15,52 +15,54 @@ function loadCSS(url) {
     document.head.appendChild(link);
 }
 
+try
+ //try start
+{
+
+    
 // Example: Load CSS from CDN
 loadCSS('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
 loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
 
 
-
-
-
-// Example: Load JS from CDN, with jQuery first and then Bootstrap
+    // Example: Load JS from CDN, with jQuery first and then Bootstrap
 loadScript('https://code.jquery.com/jquery-3.6.0.min.js', function() {
-    console.log('jQuery loaded!');
+   
 });
     // Load Bootstrap JS after jQuery has loaded
     loadScript('assets/vendor/bootstrap/js/bootstrap.bundle.min.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
     //<!-- Vendor JS Files -->
     loadScript('assets/vendor/php-email-form/validate.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
 
     loadScript('assets/vendor/aos/aos.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
 
     loadScript('assets/vendor/swiper/swiper-bundle.min.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
 
     loadScript('assets/vendor/purecounter/purecounter_vanilla.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
     loadScript('assets/vendor/waypoints/noframework.waypoints.js', function() {
-        console.log('Bootstrap JS loaded!');
+        
     });
     loadScript('assets/vendor/glightbox/js/glightbox.min.js', function() {
-        console.log('Bootstrap JS loaded!');
+        
     });
     loadScript('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
     loadScript('assets/vendor/isotope-layout/isotope.pkgd.min.js', function() {
-        console.log('Bootstrap JS loaded!');
+       
     });
     loadScript('https://raw.githack.com/jcmoralba/important_jc/main/main.js', function() {
-        console.log('Bootstrap JS loaded!');
+        
     });
     // main js file
     loadScript('assets/js/main.js', function() {      
@@ -79,5 +81,20 @@ loadScript('https://code.jquery.com/jquery-3.6.0.min.js', function() {
         loadScript('assets/js/main.js', function() {      
         });
     }
+
+
+
+    //try end
+}
+catch(e)
+{
+    console.log('have some error');
+}
+
+
+
+
+
+
 
 
